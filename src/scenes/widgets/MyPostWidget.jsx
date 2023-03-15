5eb0bac17660d5f7278a5efb3a46c9335f16d4d0
Emmanuel Dindu <83109831+emmanueldindu@users.noreply.github.com>
2023-03-15 02:40:36 +0100

    Update MyPostWidget.jsx

--- src/scenes/widgets/MyPostWidget.jsx
+++ src/scenes/widgets/MyPostWidget.jsx
@@ -57,7 +57,7 @@ function MyPostWidget({ picturePath }) {
       }
 
 
-      const response = await fetch(`https://skrt-api-backend.onrender.com/posts`, {
+      const response = await fetch(`https://skrtapi.onrender.com/posts`, {
         method: "POST",
         headers: { Authorization: `Bearer ${token}` },
         body: formData,
